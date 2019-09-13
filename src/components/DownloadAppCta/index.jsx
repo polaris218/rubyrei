b27bb6aadc25d532265fb3cb16f8d1ef9bbf74dc
reactivegeeks <software.dev0218@gmail.com>
2019-09-13 18:35:19 +0100

    Version 3.0.0b8 uploaded; size change to 835MB

--- src/components/DownloadAppCta/index.jsx
+++ src/components/DownloadAppCta/index.jsx
@@ -43,10 +43,10 @@ class DownloadAppCta extends React.Component {
             <React.Fragment>
                 <WibbuBtn className="cta-app zoom" color="pink" onClick={this._confirmDownload}>Download (PC)</WibbuBtn>
                 <DownloadModal style={{display: displaySpeedbump}}>
-                    <p>You are about to download FUN with Ruby Rei on this computer (851MB). <br/>If you don't have a login, please <a href="https://www.cambridge.org/cambridgeenglish/contact" target="_blank">contact us</a> before downloading.</p>
+                    <p>You are about to download FUN with Ruby Rei on this computer (835MB). <br/>If you don't have a login, please <a href="https://www.cambridge.org/cambridgeenglish/contact" target="_blank">contact us</a> before downloading.</p>
                     <p>Do you wish to continue?</p>
                     <footer>
-                        <a className="modal-btn" href="https://firebasestorage.googleapis.com/v0/b/ruby-rei-setup/o/FUN%20with%20Ruby%20Rei%20Setup%20v3-0-0b6.exe?alt=media&token=92ced6ab-961d-4964-8eeb-28598b68740c">YES</a>
+                        <a className="modal-btn" href="https://firebasestorage.googleapis.com/v0/b/ruby-rei-setup/o/FUN%20with%20Ruby%20Rei%20Setup%20v3-0-0b8.exe?alt=media&token=74f6b83d-9c0a-4620-9ea4-929619232ee7">YES</a>
                         <a className="modal-btn" onClick={this._cancelDownload}>NO</a>
                     </footer>
                 </DownloadModal>
